6b446e2b421db44b779751ef021d1e1b64f97032
Murat Oralbaev <successmanonly@gmail.com>
2023-03-20 20:41:17 +0300

    Gitignore added

---
+++ src/sample/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int amount = 23456;
+        int numberPerMile = 20;
+        int bonus = amount/numberPerMile;
+        System.out.println("Количество бонусных миль: " + bonus);
+    }
+}
